3e72cda127346f10bfbe6b2395725a0ea6c55e4a
Ruchi <ruchirai2300@gmail.com>
2023-02-21 15:14:00 +0530

    Initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.components';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      PropertyCardComponent,
+      PropertyListComponent,
+      NavBarComponent
+   ],
+   imports: [
+	 BrowserModule,
+   HttpClientModule
+	],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
+
